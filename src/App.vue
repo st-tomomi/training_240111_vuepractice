225d1f1377310faa0ce1aad7e16b2956390f8d34
st-tomomi <tomomi.sato@excite-software.co.jp>
2024-01-16 11:43:14 +0900

    修正_子コンポーネントのインポート

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/kimwipe.jpeg">
   <div id="app">
     <h1>Todo List</h1>
     <div class="add-todo">
-      <form @submit.prevent="addTodo" class="add-todo">
-        <input 
-          v-model="newTodo" 
-          @compositionstart="onCompositionStart" 
-          @compositionend="onCompositionEnd"
-          placeholder="新しいTodoを追加" 
-        />
-      <button @click="addTodo">+</button>
-      </form>
+      <input v-model="newTodo" placeholder="新しいTodoを追加" />
+      <button @click="addTodo">追加</button>
     </div>
     <div class="todo-list">
-      <div v-for="(todo, index) in filteredTodos" :key="index" class="todo-item">
-        <input type="checkbox" v-model="todo.completed" />
-        <span :class="{ completed: todo.completed }">{{ todo.text }}</span>
-        <button @click="deleteTodo(index)">❌</button>
-      </div>
+      <todo-item
+      v-for="todo in filteredTodos"
+      :key="todo.id"
+      :todo="todo"
+      @delete-todo="deleteTodo"
+      @toggle-completed="toggleCompleted"
+      ></todo-item>
     </div>
     <div class="filter">
-      <label>
-        <button @click="switchShowCompleted">
-          {{ showCompleted ? 'Hide completed' : 'Show All' }}
-        </button>
-      </label>
+      <button @click="toggleShowCompleted">
+      {{ showCompleted ? 'Hide Completed' : 'Show All' }}
+      </button>
     </div>
+
   </div>
 </template>
 
 <script>
+import TodoItem from './components/TodoItem.vue';
+
 export default {
+  components: {
+    TodoItem
+  },
   data() {
     return {
       newTodo: '',
       todos: [],
       showCompleted: true,
-      isComposing: false, /*文字入力の変換中かどうか*/
+      nextTodoId: 1
     };
   },
   computed: {
@@ -47,24 +45,28 @@ export default {
   },
   methods: {
     addTodo() {
-      if (!this.isComposing && this.newTodo.trim() !== '') {
-        this.todos.push({ text: this.newTodo, completed: false });
+      if (this.newTodo.trim() !== '') {
+        this.todos.push({
+          id: this.nextTodoId++,
+          text: this.newTodo,
+          completed: false
+        });
         this.newTodo = '';
       }
     },
-    deleteTodo(index) {
-      this.todos.splice(index, 1);
+    deleteTodo(todoId) {
+      this.todos = this.todos.filter(todo => todo.id !== todoId);
     },
-    switchShowCompleted() {
+    toggleCompleted(todoId) {
+      const index = this.todos.findIndex(todo => todo.id === todoId);
+      if (index !== -1) {
+        this.todos[index].completed = !this.todos[index].completed;
+      }
+    },
+    toggleShowCompleted() {
       this.showCompleted = !this.showCompleted;
-    },
-    onCompositionStart() {
-      this.isComposing = true;
-    },
-    onCompositionEnd() {
-      this.isComposing = false;
-    },
-  },
+    }
+  }
 };
 </script>
 
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="todo-item">
+      <input type="checkbox" :checked="todo.completed" @change="toggleCompleted" />
+      <span :class="{ completed: todo.completed }">{{ todo.text }}</span>
+      <button @click="deleteTodo">削除</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      todo: Object
+    },
+    methods: {
+      deleteTodo() {
+        this.$emit('delete-todo', this.todo.id);
+      },
+      toggleCompleted() {
+        this.$emit('toggle-completed', this.todo.id);
+      }
+    }
+  }
+  </script>
+  
+  <!-- スタイル -->
+  <style>
+  .completed {
+    text-decoration: line-through;
+  }
+  </style>
+  
